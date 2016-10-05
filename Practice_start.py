96352973f8559075814bc05fbd9a2c2664aeb9cb
vermanil <anilrajverma1996@gmail.com>
2016-10-05 11:07:14 +0530

    Add some required features

--- Practice_start.py
+++ Practice_start.py
@@ -30,6 +30,22 @@ class Ui_Form(object):
         self.label.setGeometry(QtCore.QRect(170, 280, 50, 14))
         self.label.setObjectName(_fromUtf8("label"))
 
+        #Add progress bar
+        self.progressBar = QtGui.QProgressBar(Form)
+        self.progressBar.setGeometry(QtCore.QRect(230, 40, 118, 23))
+        self.progressBar.setMinimumSize(QtCore.QSize(118, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(5)
+
+        #Add text box
+        self.timeToStart = QtGui.QTextEdit(Form)
+        self.timeToStart.setGeometry(QtCore.QRect(80, 40, 111, 31))
+        self.timeToStart.setObjectName(_fromUtf8("timeToStart"))
+        self.timeToStart.setReadOnly(True)
+        self.timeToStart.setText(" Start in ")
+
+        # Add show text paragraph
         self.showPara = QtGui.QTextEdit(Form)
         self.st = "Anil"
         self.showPara.setText(self.st)
@@ -37,27 +53,23 @@ class Ui_Form(object):
         self.showPara.setObjectName(_fromUtf8("showPara"))
         self.showPara.setReadOnly(True)
 
-
+        # Add Edit text box
         self.editPara = QtGui.QTextEdit(Form)
         self.editPara.setGeometry(QtCore.QRect(170, 300, 371, 121))
         self.editPara.setObjectName(_fromUtf8("editPara"))
+        self.editPara.setReadOnly(True)
         self.editPara.textChanged.connect(self.text_changed)
-        #self.editPara.textCursor().insertHtml('normal text')
-        #self.editPara.setReadOnly(True)
-        #self.mytext = self.editPara.toPlainText()
-        #QString *mytext = self.editPara->toPlainText();
-        #print self.mytext
-        #self.editPara.setReadOnly(False)
-        #self.editPara.textCursor().insertHtml('<b></b>')
 
         self.label_2 = QtGui.QLabel(Form)
         self.label_2.setGeometry(QtCore.QRect(170, 110, 81, 16))
         self.label_2.setObjectName(_fromUtf8("label_2"))
 
+        # add Ok button
         self.finish = QtGui.QPushButton(Form)
         self.finish.setGeometry(QtCore.QRect(490, 510, 51, 27))
         self.finish.setObjectName(_fromUtf8("finish"))
 
+        # Add cancel button
         self.Cancel = QtGui.QPushButton(Form)
         self.Cancel.setGeometry(QtCore.QRect(570, 510, 51, 27))
         self.Cancel.setObjectName(_fromUtf8("Cancel"))
@@ -67,7 +79,7 @@ class Ui_Form(object):
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(_translate("Form", "Form", None))
+        Form.setWindowTitle(_translate("Form", "Game TypeRacer", None))
         self.label.setText(_translate("Form", "Type Here", None))
         self.label_2.setText(_translate("Form", "Your Paragraph", None))
         self.finish.setText(_translate("Form", "Finish", None))
@@ -77,29 +89,54 @@ class Ui_Form(object):
         self.editPara.setReadOnly(True)
         mytext = self.editPara.toPlainText()
         l = len(mytext)
-        print l
-        print self.st[0:l]
+        #print l
+        #print self.st[0:l]
         if mytext == self.st[0:l]:
             print "match"
+            self.editPara.setStyleSheet("QTextEdit {color:black}")
             #self.editPara.textCursor().insertHtml('<b>mytext</b>')
         #print mytext
         else:
-            self.editPara.setTextColor(GlobalColor('255,0,0'));
+            self.editPara.setStyleSheet("QTextEdit {color:red}")
 
         self.editPara.setReadOnly(False)
 
+    #function to update progress bar
+    def update_progressbar(self, val):
+        self.progressBar.setValue(val)
+        if val == 5:
+            self.timeToStart.setText("Go..Game will start ")
+            self.timeToStart.setStyleSheet("QTextEdit {color:green}")
+
+    #function to directly close App 
     def closeApp(self):
         print "Cancel pressed"
         app.exit()
 
 
+    #function to start game in 4sec
+    def startIn(self):
+        if timer.q < 6:
+            self.update_progressbar(timer.q)
+            print 'tick'
+            timer.q += 1
+        else:
+            self.editPara.setReadOnly(False)
 
 if __name__ == "__main__":
     import sys
+    from PyQt4.QtCore import QTimer
     app = QtGui.QApplication(sys.argv)
     Form = QtGui.QWidget()
     ui = Ui_Form()
     ui.setupUi(Form)
     Form.show()
+
+    #set timer
+    timer = QTimer()
+    timer.q = 0
+    timer.timeout.connect(ui.startIn)
+    timer.start(1000)
+
     sys.exit(app.exec_())
 
