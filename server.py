5a5c5cc030c8c503c0f35efd3bbbd229523203ee
manish1996 <singla.iiitv@gmail.com>
2016-10-07 20:20:29 +0530

    optimised version for server side

--- server.py
+++ server.py
@@ -1,13 +1,52 @@
 # server side program
 
-import socket              
-s = socket.socket()         
-host = socket.gethostname() 
-port = 12345                
-s.bind((host, port))        
-s.listen(5)                 
-while True:
-   c, addr = s.accept()     
-   print 'Got connection from', addr
-   c.send('you are connected')
-   c.close()               
+import socket 
+import sys
+
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host=""
+        port=12345
+        s=socket.socket()
+    except socket.error as tag:
+        print str(tag)
+
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+        s.bind((host,port))
+        s.listen(5)
+    except socket.error as tag:
+        print str(tag)
+        bind_socket()
+
+def accept_socket():
+    c, addr=s.accept()
+    print "connected client is"+"IP" +addr[0] + "port" +addr[1]
+    send_msg(c)
+    c.close()
+
+def send_msg(c):
+    while(true):
+        m=raw_input()
+        if(m=='quit'):
+            c.close()
+            s.close()
+            sys.exit()
+        if(len(str.encode(m)>0)):
+            c.send(str.encode(m))
+            client_responce=str(c.recv(1024),"utf-8")
+            print client_responce
+        
+def main():
+    create_socket()
+    bind_socket()
+    accept_socket()
+
+main()        
+           
