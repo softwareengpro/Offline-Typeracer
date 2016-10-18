cb2df670d3b40ddd9a76e3d7f98452ea0829d7a1
vermanil <anilrajverma1996@gmail.com>
2016-10-18 10:45:05 +0530

    update connection between gui

---
+++ selectSession.py
@@ -0,0 +1,92 @@
+from PyQt4 import QtCore, QtGui
+import Practice_start
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(700, 500)
+        self.practice_session = QtGui.QPushButton(Form)
+        self.practice_session.setGeometry(QtCore.QRect(180, 220, 85, 27))
+        self.practice_session.setObjectName(_fromUtf8("practice_session"))
+
+
+        self.create_challenge = QtGui.QPushButton(Form)
+        self.create_challenge.setGeometry(QtCore.QRect(440, 220, 101, 27))
+        self.create_challenge.setObjectName(_fromUtf8("create_challenge"))
+
+        self.acceptchallenge = QtGui.QPushButton(Form)
+        self.acceptchallenge.setGeometry(QtCore.QRect(310, 310, 101, 27))
+        self.acceptchallenge.setObjectName(_fromUtf8("acceptchallenge"))
+
+        self.back = QtGui.QPushButton(Form)
+        self.back.setGeometry(QtCore.QRect(40, 440, 51, 27))
+        self.back.setObjectName(_fromUtf8("back"))
+
+        self.close = QtGui.QPushButton(Form)
+        self.close.setGeometry(QtCore.QRect(610, 440, 51, 27))
+        self.close.setObjectName(_fromUtf8("close"))
+        self.close.clicked.connect(self.closeApp)
+
+
+        self.label = QtGui.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(270, 110, 161, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setUnderline(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.obj = Form
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Select Mode", None))
+        self.practice_session.setText(_translate("Form", "Practice", None))
+        self.create_challenge.setText(_translate("Form", "Create Challenge", None))
+        self.acceptchallenge.setText(_translate("Form", "Accept Challenge", None))
+        self.back.setText(_translate("Form", "Back", None))
+        self.close.setText(_translate("Form", "close", None))
+        self.label.setText(_translate("Form", "Choose Your Session", None))
+
+    def closeApp(self):
+        print "Cancel pressed"
+        sys.exit()
+
+    def ready(self):
+        self.obj.hide()
+        sec = Practice_start.Ui_Form()
+        sec.setupUi(self.obj)
+        self.obj.show()
+
+        sec.close.clicked.connect(self.closeApp)
+
+
+
+"""if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())"""
+
---
+++ startGame.py
@@ -0,0 +1,113 @@
+from PyQt4 import QtCore, QtGui
+#from selectSession import Ui_Form
+import selectSession, Practice_start
+from PyQt4.QtCore import QTimer
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_O(object):
+    def setupUi(self, O):
+        O.setObjectName(_fromUtf8("O"))
+        O.resize(700, 500)
+
+        self.centralwidget = QtGui.QWidget(O)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.startgame = QtGui.QPushButton(self.centralwidget)
+        self.startgame.setGeometry(QtCore.QRect(330, 200, 85, 27))
+        self.startgame.setObjectName(_fromUtf8("startgame"))
+        #self.startgame.clicked.connect(self.newWindow)
+
+
+        self.close = QtGui.QPushButton(self.centralwidget)
+        self.close.setGeometry(QtCore.QRect(330, 270, 85, 27))
+        self.close.setObjectName(_fromUtf8("close"))
+        
+
+        self.retranslateUi(O)
+        QtCore.QMetaObject.connectSlotsByName(O)
+
+        self.obj = O
+
+    def retranslateUi(self, O):
+        O.setWindowTitle(_translate("O", "OFFLINE TYPERACER", None))
+        self.startgame.setText(_translate("O", "StarGame", None))
+        self.close.setText(_translate("O", "Close", None))
+        self.close.clicked.connect(self.closeApp)
+        self.startgame.clicked.connect(self.selectMode)
+
+    def closeApp(self):
+        print "Cancel pressed"
+        #choice = QtGui.QMessageBox.question(self,'Extract!',"Get into the chopper?",QtGui.QMessageBox.Ok | QtGui.QMessageBox.No)
+        #if choice == QMessageBox.Ok:
+        sys.exit()
+        #else:
+        #    pass
+        #app.quit()
+
+    def selectMode(self):
+        self.obj.hide()
+        #layout = QtGui.QHBoxLayout(O)
+        #s = QtGui.QWidget()
+        #layout.removeWidget(self.obj)
+
+        sec = selectSession.Ui_Form()
+        sec.setupUi(self.obj)
+        self.obj.show()
+        self.startgame.hide()
+        self.close.hide()
+        sec.close.clicked.connect(self.closeApp)   
+        sec.practice_session.clicked.connect(self.sessionPractice)
+
+    def sessionPractice(self):
+    	self.obj.hide()
+    	prac = Practice_start.Ui_Form()
+    	prac.setupUi(self.obj)
+    	self.obj.show()
+    	timer = QTimer()
+    	timer.q = 0
+    	timer.timeout.connect(prac.startIn)
+    	timer.start(1000)
+
+        # app.exit()
+
+    """def newWindow(self):
+                        #import sys
+                        #app = QtGui.QApplication(sys.argv)
+                        Form = QtGui.QWidget()
+                        ui = selectSession.Ui_Form()
+                        ui.setupUi(Form)
+                        Form.show()
+                        #O.hide()
+                        sys.exit(app.exec_())"""
+
+if __name__ == "__main__":
+    import sys
+    #import selectSession
+    app = QtGui.QApplication(sys.argv)
+    O = QtGui.QWidget()
+    ui = Ui_O()
+    ui.setupUi(O)
+    O.show()
+    #layout = QtGui.QHBoxLayout(O)
+    #print layout
+    app.exec_()
+    #print "hi"
+    # app = QtGui.QApplication(sys.argv)
+    # O = QtGui.QWidget()
+    # sec = Ui_Form()
+    # sec.setupUi(O)
+    # O.show()
+    # app.exec_()
+    sys.exit(0)
