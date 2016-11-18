54226bc36039cda16b10fa8375fa4c562b357a32
vrmanil <anilrajverma1996@gmail.com>
2016-11-19 01:12:28 +0530

    add innstructionn mannual

--- startGame.py
+++ startGame.py
@@ -7,7 +7,7 @@ from PyQt4.QtCore import QTimer
 import feedparser
 import urllib2
 import audio_edit, Join
-from PySide.phonon import Phonon
+#from PySide.phonon import Phonon
 import socket
 import threading
 
@@ -92,6 +92,10 @@ class Ui_O(object):
         self.Score.setGeometry(QtCore.QRect(470, 400, 111, 27))
         self.Score.setStyleSheet(_fromUtf8("color: rgb(0, 0, 49);"))
         self.Score.setObjectName(_fromUtf8("Score"))
+        self.Instrution = QtGui.QPushButton(self.centralwidget)
+        self.Instrution.setGeometry(QtCore.QRect(330, 500, 111, 27))
+        self.Instrution.setStyleSheet(_fromUtf8("color: rgb(0, 0, 49);"))
+        self.Instrution.setObjectName(_fromUtf8("Instrution"))
         #O.setCentralWidget(self.centralwidget)
         # self.statusbar = QtGui.QStatusBar(O)
         # self.statusbar.setObjectName(_fromUtf8("statusbar"))
@@ -109,6 +113,7 @@ class Ui_O(object):
         self.showUserName.setText(_translate("O", "      Hello asdfg", None))
         self.changeUserName.setText(_translate("O", "Change Username", None))
         self.Score.setText(_translate("O", "Score", None))
+        self.Instrution.setText(_translate("0", "Instruction", None))
 
         self.close.clicked.connect(self.closeApp)
         if userText != '': 
@@ -153,6 +158,7 @@ class Ui_O(object):
         self.close.hide()
         self.showUserName.hide()
         self.changeUserName.hide()
+        self.Instrution.hide()
         self.Score.hide()
         sessionMode.close.clicked.connect(self.closeApp) 
         sessionMode.practice_session.clicked.connect(self.selectTopic)  
@@ -206,10 +212,14 @@ class Ui_O(object):
         print s
         if s==5:
             print 'server closed'
+            self.s.close()
+            print self.s
             timer.stop()
-            if Client1 == '':
+            if Client == 1:
                 print 'No one connected'
-            #startedServer.close()
+            else:
+                print "get a connection"
+
 
     def serverCreate(self):
         self.timer1()
@@ -219,6 +229,7 @@ class Ui_O(object):
 
 
     def MyThread1(self):
+        global Client
         Client, Adr=(self.s.accept())
         print 'Got a connection from: ' + str(Client) + '.'
         self.multiType()
@@ -263,8 +274,22 @@ class Ui_O(object):
         timeChallenge.Min1.clicked.connect(self.challengeCreate)
         timeChallenge.Min2.clicked.connect(self.challengeCreate)
         timeChallenge.Min3.clicked.connect(self.challengeCreate)
-        timeChallenge.back.clicked.connect(self.backTime_word)
+        timeChallenge.Back.clicked.connect(self.backTime_wordChallenge)
         self.obj.show()
+
+    def backTime_wordChallenge(self):
+        self.obj.hide()
+        wordMapChallenge.setupUi(self.obj)
+        timeChallenge.Min1.hide()
+        timeChallenge.Min2.hide()
+        timeChallenge.Min3.hide()
+        timeChallenge.TimeMap.hide()
+        wordMapChallenge.Paragraph.clicked.connect(self.timeMapChallenge)
+        wordMapChallenge.News.clicked.connect(self.challengeCreate)
+        wordMapChallenge.Gk.clicked.connect(self.challengeCreate)
+        wordMapChallenge.Back.clicked.connect(self.backword_session)
+        self.obj.show()
+
 
     def joinChallenge(self):
         #self.obj.hide()
@@ -283,6 +308,14 @@ class Ui_O(object):
         joinChallenge.joinchallenge.hide()
         joinChallenge.textEdit.hide()
         joinChallenge.label.hide()
+        multiTypeObject.setupUi(self.obj)
+        self.obj.show()
+
+    def multiType1(self):
+        self.obj.hide()
+        # joinChallenge.joinchallenge.hide()
+        # joinChallenge.textEdit.hide()
+        # joinChallenge.label.hide()
         multiTypeObject.setupUi(self.obj)
         self.obj.show()
 
@@ -757,6 +790,7 @@ class Ui_O(object):
 
     #initial activity
     def firstActivity(self):
+        self.obj.hide()
         prac_sess.progressBar.hide()
         prac_sess.timeToStart.hide()
         prac_sess.showPara.hide()
@@ -767,6 +801,9 @@ class Ui_O(object):
         self.setupUi(self.obj)
         self.startgame.show()
         self.close.show()
+        self.changeUserName.clicked.connect(self.enterUsername)
+        self.Score.clicked.connect(self.resultWindow)
+        self.Instrution.clicked.connect(self.typingManual)
         self.obj.show()
 
     def begin_time(self):
@@ -823,6 +860,10 @@ class Ui_O(object):
         print showUser
         self.showUserName.setText(str('   ' + 'Hello' + ' ' + showUser))
 
+    def typingManual(self):
+        import webbrowser
+        webbrowser.open_new(r'Typing_manual.pdf')
+
 
 """    def startInFive(self):
         d = QtGui.QDialog()
@@ -843,7 +884,6 @@ if __name__ == "__main__":
     file = open("username.txt","rw+")
     userText = file.read()
     wrongType = []
-    Client1 = ''
     i = 2
     input_l = 0
     para_l = 0
@@ -852,6 +892,7 @@ if __name__ == "__main__":
     result = ''
     result1 = ''
     result2 = ''
+    Client = 1
 
     start_time = time()
     print start_time
@@ -862,6 +903,7 @@ if __name__ == "__main__":
     ui.showName()
     ui.changeUserName.clicked.connect(ui.enterUsername)
     ui.Score.clicked.connect(ui.resultWindow)
+    ui.Instrution.clicked.connect(ui.typingManual)
     O.show()
 
     #ui.update_currentAffairs()
